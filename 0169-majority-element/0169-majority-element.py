e5a8f76f6a3e446c8f89883d4e7bbd8920485218
Anna Wu <121405231+annacwu@users.noreply.github.com>
2023-11-30 12:22:35 -0800

    Time: 108 ms (97.60%), Space: 14.8 MB (60.41%) - LeetHub

---
+++ 0169-majority-element/0169-majority-element.py
@@ -0,0 +1,53 @@
+class Solution(object):
+    def majorityElement(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+
+        nums.sort()
+        return nums[len(nums)/2]
+
+
+        # checked = set()
+        # count = 0
+        # check = random.choice(nums)
+        # done = False
+        # while not done: 
+            
+
+
+        # count = 0
+        # def checking(check):
+        #     count = 0
+        #     for elem in nums:
+        #         if check == elem: 
+        #             count += 1
+        #     return count
+
+        # if count == len(nums) / 2:
+        #     return check
+        # else: 
+        #     checked = set()
+        #     check = random.choice(nums)
+        #     while check in checked: 
+        #         check = random.choice(nums)
+        #     checked.add(check)
+
+        #     checking(check)
+
+        # return count
+
+
+        # n = len(nums)
+        # d = {}
+        # majority =
+        # for elem in nums: 
+        #     if elem in d: 
+        #         d[elem] += 1
+        #     else: 
+        #         d[elem] = 0
+
+        # for item in d:
+        #     if item > n / 2:
+        #         return item
